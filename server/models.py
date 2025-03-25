45bc4a9849bdb96e45417cc9f5e76fd11e19f39d
tillenotuoma <totuoma27@gmail.com>
2025-03-25 23:41:29 +0300

    done

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from models import db, Pet
 
 # contains definitions of tables and associated schema constructs
 metadata = MetaData()
